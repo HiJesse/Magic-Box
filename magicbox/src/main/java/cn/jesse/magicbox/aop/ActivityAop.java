c46c397f458de1f06dc7a70d94c852b84bf2809c
Jesse <lenjoyshow@gmail.com>
2020-07-12 16:11:44 +0800

    feat: 精简activity切面

--- magicbox/src/main/java/cn/jesse/magicbox/aop/ActivityAop.java
+++ magicbox/src/main/java/cn/jesse/magicbox/aop/ActivityAop.java
@@ -5,7 +5,6 @@ import android.util.Log;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * activity aop
@@ -18,37 +17,27 @@ public class ActivityAop {
 
     /**
      * onCreate 切入点
+     *
+     * @param joinPoint 切入点
+     * @throws Throwable e
      */
-    @Pointcut("execution(* android.app.Activity.onCreate(..))")
-    public void activityOnCreate() {
-        // unused
-    }
-
-
-    @Around("activityOnCreate()")
+    @Around("execution(void android.app.Activity.onCreate(android.os.Bundle))")
     public void activityOnCreateTriggered(ProceedingJoinPoint joinPoint) throws Throwable {
-        String targetClassName = joinPoint.getTarget().getClass().getName();
-        String signatureName = joinPoint.getSignature().getName();
         long beforeTime = System.currentTimeMillis();
         joinPoint.proceed();
-        Log.d(TAG, targetClassName + "." + signatureName + " duration: " + (System.currentTimeMillis() - beforeTime));
+        Log.d(TAG, joinPoint.getThis().hashCode() + ".onCreate duration: " + (System.currentTimeMillis() - beforeTime));
     }
 
     /**
      * onDestroy 切入点
+     *
+     * @param joinPoint 切入点
+     * @throws Throwable e
      */
-    @Pointcut("execution(* android.app.Activity.onDestroy())")
-    public void activityOnDestroy() {
-        // unused
-    }
-
-
-    @Around("activityOnDestroy()")
+    @Around("execution(void android.app.Activity.onDestroy())")
     public void activityOnDestroyTriggered(ProceedingJoinPoint joinPoint) throws Throwable {
-        String targetClassName = joinPoint.getTarget().getClass().getName();
-        String signatureName = joinPoint.getSignature().getName();
         long beforeTime = System.currentTimeMillis();
         joinPoint.proceed();
-        Log.d(TAG, targetClassName + "." + signatureName + " duration: " + (System.currentTimeMillis() - beforeTime));
+        Log.d(TAG, joinPoint.getThis().hashCode() + ".onDestroy duration: " + (System.currentTimeMillis() - beforeTime));
     }
 }
